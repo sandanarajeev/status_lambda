77afe9642157fbec045b63b77f832582a70b5e36
sandanarajeev <sandanarajeev@gmail.com>
2022-08-04 00:48:23 +0530

    INIT

---
+++ status_sns.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import boto3
+
+AWS_REGION = ""
+ec2_id = ""
+rds_id = ""
+topic_arn = ""
+
+rdsKey = "test"
+rdsValue = "abiola-ground"
+
+ec2Key = "test"
+ec2Value = "abiola-ground"
+
+
+def lambda_handler(event, context):
+
+    client = boto3.client("rds", region_name=AWS_REGION)
+    response = client.describe_db_instances()
+
+    message = "RDS\n"
+    for instance in response["DBInstances"]:
+        rdsInstance = None
+        if(instance["DBInstanceIdentifier"] == rds_id):
+            rdsInstance = instance
+        else:
+            tags = instance["TagList"]
+            tag = next(
+                (tag for tag in tags if tag["Key"] == rdsKey and tag["Value"] == rdsValue), None)
+            if(tag != None):
+                rdsInstance = instance
+        if(rdsInstance):
+            message = message + f"""
+Instance {rdsInstance["DBInstanceIdentifier"]} information:\n
+State: {rdsInstance["DBInstanceStatus"]}\n"""
+
+    print("rdsInstance", rdsInstance)
+
+    message = message + "\nEC2\n"
+
+    ec2 = boto3.resource("ec2", region_name=AWS_REGION)
+    ec2_resources = ec2.instances.all()
+
+    for instance in ec2_resources:
+        ec2Instance = None
+        print("instance", instance)
+        if(instance.id == ec2_id):
+            ec2Instance = instance
+        else:
+            tags = instance.tags
+            tag = next(
+                (tag for tag in tags if tag["Key"] == ec2Key and tag["Value"] == ec2Value), None)
+        if(tag != None):
+            ec2Instance = instance
+        if(ec2Instance):
+            message = message + f"""
+Instance {ec2Instance.id} information:\n
+State: {ec2Instance.state["Name"]}\n"""
+
+    print("ec2Instance", ec2Instance)
+
+    subject = "Status Mail"
+    print("message", message)
+    sns = boto3.client("sns", region_name=AWS_REGION)
+    sns.publish(TopicArn=topic_arn, Message=message, Subject=subject)
+
+    return {
+        "StatusCode": 200,
+        "Body": "Success"
+    }
+
+
+# enable for local testing
+# lambda_handler(None, None)
